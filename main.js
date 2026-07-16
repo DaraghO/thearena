4c12be96f910096aa1b8dc05fdf546ec871c196e
DaraghO <94379706+DaraghO@users.noreply.github.com>
2026-07-16 11:55:22 +0100

    Implement real-time room creation and display

---
+++ main.js
@@ -0,0 +1,57 @@
+import { db, login } from "./firebase.js";
+
+import {
+    collection,
+    onSnapshot,
+    addDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+await login();
+
+
+const roomsDiv = document.getElementById("rooms");
+const createButton = document.getElementById("createRoom");
+
+
+// Listen for rooms
+
+onSnapshot(collection(db, "rooms"), (snapshot)=>{
+
+    roomsDiv.innerHTML = "";
+
+    snapshot.forEach((doc)=>{
+
+        let room = doc.data();
+
+        let button = document.createElement("button");
+
+        button.innerText = room.name;
+
+        roomsDiv.appendChild(button);
+
+    });
+
+});
+
+
+// Create room
+
+createButton.onclick = async ()=>{
+
+    await addDoc(collection(db, "rooms"), {
+
+        name: "Room " + Math.floor(Math.random()*10000),
+
+        host: crypto.randomUUID(),
+
+        player2: null,
+
+        state: "waiting",
+
+        created: serverTimestamp()
+
+    });
+
+};
