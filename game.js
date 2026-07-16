cb58d0a3bdbc37a69bb377bbcc5b0a5dbfd48c4f
DaraghO <94379706+DaraghO@users.noreply.github.com>
2026-07-16 14:31:13 +0100

    Implement startGame function with Firebase snapshot
    Added Firebase integration to start game functionality.

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
+import { db, auth } from "./firebase.js";
 
+import {
+    doc,
+    onSnapshot,
+    updateDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+export function startGame(roomId)
+{
+    const roomRef = doc(db, "rooms", roomId);
+
+    onSnapshot(roomRef, async (snapshot)=>{
+
+        if(!snapshot.exists())
+            return;
+
+        const room = snapshot.data();
+
+        if(room.state !== "playing")
+            return;
+
+        console.log("Game running", room);
+
+    });
+}
