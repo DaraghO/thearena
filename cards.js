4df2dde1a39e2d1364400eb25c6ff5f0bd3dd6f4
DaraghO <94379706+DaraghO@users.noreply.github.com>
2026-07-21 21:41:28 +0100

    Enhance archer card with additional targeting properties
    Added new properties 'adjacentLaneTargeting' and 'attackLockedTargetAtAnyRange' to the 'archer' card.

--- cards.js
+++ cards.js
@@ -2,8 +2,22 @@
 export const cards = [
     { id:"knight",  name:"Knight",  type:"unit", rarity:"common",
       cost:2, health:500, damage:80, speed:4, range:1, detectRange:3, attackSpeed:1 },
-    { id:"archer",  name:"Archers", type:"unit", rarity:"common",
-      cost:2, health:250, damage:50, speed:7, range:5, detectRange:6, attackSpeed:1.5 },
+    {
+    id:"archer",
+    name:"Archers",
+    type:"unit",
+    rarity:"common",
+    cost:2,
+    health:250,
+    damage:50,
+    speed:7,
+    range:5,
+    detectRange:6,
+    attackSpeed:1.5,
+
+    adjacentLaneTargeting:true,
+    attackLockedTargetAtAnyRange:true
+},
     { id:"fireball", name:"Fireball", type:"spell", rarity:"rare",
       cost:5, damage:300, range:3 },
     { id:"healing_potion", name:"Healing Potion", type:"item", rarity:"rare",
