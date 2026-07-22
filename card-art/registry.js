fde08056646dde7e2c438e969208b3e3ac1b7132
DaraghO <94379706+DaraghO@users.noreply.github.com>
2026-07-22 02:14:35 +0100

    Add gold miner rig to card art registry

--- card-art/registry.js
+++ card-art/registry.js
@@ -2,7 +2,9 @@
 // The key must exactly match the card's id in cards.js.
 
 import { archerRig } from "./archer/rig.js";
+import { goldMinerRig } from "./gold-miner/rig.js";
 
 export const CARD_ART_RIGS = {
-    archer: archerRig
+    archer: archerRig,
+    gold_miner: goldMinerRig
 };
